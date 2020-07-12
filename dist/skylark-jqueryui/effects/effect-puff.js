ebbae1f9f2708cd435523f639cd06a16b1d40099
hudaokeji <github@hudaokeji.com>
2020-07-12 17:18:51 +0900

    update

--- dist/skylark-jqueryui/effects/effect-puff.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-jqueryui - A version of backbone that ported to running on skylarkjs
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-integration/skylark-jqueryui/
- * @license MIT
- */
-define(["skylark-domx-effects/plugins/puff"],function(f){return f});
-//# sourceMappingURL=../sourcemaps/effects/effect-puff.js.map
