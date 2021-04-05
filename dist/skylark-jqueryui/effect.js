2cb4829d1c6e3964ad93918102f11cca8e8763d9
hudaokeji <github@hudaokeji.com>
2021-04-05 16:59:07 +0900

    update

---
+++ dist/skylark-jqueryui/effect.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-jqueryui - A version of backbone that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-jqueryui/
+ * @license MIT
+ */
+define(["skylark-jquery","skylark-domx-effects/effects"],function(e,f){return e.effects=f});
+//# sourceMappingURL=sourcemaps/effect.js.map
